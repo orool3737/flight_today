7d71dda3185e9291845540af0e56efe9031b749a
orool3737 <orool3737@naver.com>
2021-08-16 12:41:25 +0900

    Update flight_hiair_today.py

--- flight_hiair_today.py
+++ flight_hiair_today.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from cralwer_flight_hiair_today import exract_flight_hiair_today
 
-first_content = exract_flight_hiair_monday('16','0')
+first_content = exract_flight_hiair_today('16','0')
  
 bot = telegram.Bot(token=os.environ.get('telegram_token'))
 chat_id = 1491027495 #bot.getUpdates()[-1].message.chat.id
